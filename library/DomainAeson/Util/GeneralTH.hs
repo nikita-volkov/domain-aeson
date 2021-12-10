11e3cd559aa931cd8442dc6d6040f3a86b36ef35
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-12-10 17:15:14 +0300

    Clean up

--- library/DomainAeson/Util/GeneralTH.hs
+++
@@ -1,7 +0,0 @@
-module DomainAeson.Util.GeneralTH where
-
-import qualified Data.Text as Text
-import DomainAeson.Prelude
-import Language.Haskell.TH.Syntax
-import THLego.Helpers
-import qualified THLego.Lambdas as Lambdas
