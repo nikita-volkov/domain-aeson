39e9d96624ed74cf06f5405f7d4ee40bd04eb394
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-12-10 18:02:51 +0300

    Finish toJson

--- library/DomainAeson/InstanceDecs.hs
+++ library/DomainAeson/InstanceDecs.hs
@@ -7,6 +7,7 @@ import qualified DomainCore.TH as DomainTH
 import Language.Haskell.TH.Syntax
 import THLego.Helpers
 
+toJson :: Model.TypeDec -> Dec
 toJson (Model.TypeDec typeName typeDef) =
   case typeDef of
     Model.ProductTypeDef members ->
@@ -14,3 +15,13 @@ toJson (Model.TypeDec typeName typeDef) =
         (ConT (textName typeName))
         (textName typeName)
         (fmap fst members)
+    Model.SumTypeDef members ->
+      AesonTH.sumToJsonInstanceDec
+        (ConT (textName typeName))
+        (fmap member members)
+      where
+        member (memberName, memberComponentTypes) =
+          ( memberName,
+            DomainTH.sumConstructorName typeName memberName,
+            length memberComponentTypes
+          )
